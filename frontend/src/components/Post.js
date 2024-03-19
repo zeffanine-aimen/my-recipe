4b827faead267590197f996dc8e2231894d04c06
Zeffanine Aimen <zeffanine16@gmail.com>
2024-03-19 09:38:57 +0100

    work on the UI of the Post

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -1,17 +1,32 @@
-// Post.js
-
 import React from 'react';
+import { RiUserFill } from 'react-icons/ri';
+import moment from 'moment';
+import '../styles/Post.css'; // Import CSS file for styling
 
 function Post({ post }) {
+  const firstImage = post.Post_Images.length > 0 ? post.Post_Images[0] : null;
+  const updatedAt = moment(post.updatedAt);
+
   return (
-    <div>
-      <h3>{post.title}</h3>
-      <p>{post.contents}</p>
-      <p>User: {post.User.name}</p>
-      {post.Post_Images.map(image => (
-        <img key={image.id} src={image.url} alt={image.alt} />
-      ))}
-      {/* Render other post details as needed */}
+    <div className="post-card">
+      {firstImage && (
+        <div className="post-image-container">
+          <img src={`${process.env.REACT_APP_API_URL}${firstImage.img_uri}`} alt={firstImage.alt} className="post-image" />
+        </div>
+      )}
+      <div className="user-info">
+        {post.User.img_uri ? (
+          <img src={`${process.env.REACT_APP_API_URL}${post.User.img_uri}`} alt="User Avatar" className="user-avatar" />
+        ) : (
+          <div className="user-avatar"><RiUserFill size="2em" /></div>
+        )}
+        <div className="user-details">
+          <p className="user-name">{post.User.name}</p>
+          <p className="time">{updatedAt.fromNow()}</p>
+        </div>
+      </div>
+      <h3 className="post-title">{post.title}</h3>
+      <p className="post-content">{post.contents}</p>
     </div>
   );
 }
